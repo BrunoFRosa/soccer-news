bba9900f76e2994538f99fc3b94aa4a685e1a041
BrunoFRosa <fonsecabruno98@gmail.com>
2022-07-24 22:59:50 -0300

    first commit

--- app/src/main/java/com/example/soccernews/doman/News.java
+++ app/src/main/java/com/example/soccernews/doman/News.java
@@ -13,15 +13,12 @@ public class News {
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
